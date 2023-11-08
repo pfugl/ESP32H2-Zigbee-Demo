f0b4ccd8d8f18320aad291bf29aec2acf2a6e639
Peter Fugl <peter@pfugl.dk>
2023-11-08 15:49:48 +0100

    no changes

--- main/esp_zb_light.h
+++ main/esp_zb_light.h
@@ -1,7 +1,7 @@
 #include "esp_zigbee_core.h"
 
 /* Zigbee configuration */
-#define INSTALLCODE_POLICY_ENABLE false /* enable the install code policy for security */
+#define INSTALLCODE_POLICY_ENABLE false  /* enable the install code policy for security */
 #define ED_AGING_TIMEOUT ESP_ZB_ED_AGING_TIMEOUT_64MIN
 #define ED_KEEP_ALIVE 3000                                               /* 3000 millisecond */
 #define HA_ESP_LIGHT_ENDPOINT 10                                         /* esp light bulb device endpoint, used to process light controlling commands */
